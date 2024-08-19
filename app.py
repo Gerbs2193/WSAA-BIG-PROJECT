e48b707cd4f8338f4ae6326034ebc309f8448579
gerball <gerball2193@gmail.com>
2024-08-19 21:38:20 +0100

    Changing topic

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fishkeeping.db'
 db = SQLAlchemy(app)
 
-class Movie(db.Model):
+class Fish(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    genre = db.Column(db.String(50), nullable=False)
-    status = db.Column(db.String(20), nullable=False)
+    species_name = db.Column(db.String(100), nullable=False)
+    tank_name = db.Column(db.String(100), nullable=False)
+    date_added = db.Column(db,Date, nullable)=False, default=date.today
+    notes = db.Column(db.Column(db.Text, nullable=True)
 
 with app.app_context():
     db.create_all()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
