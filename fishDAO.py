0ecfa0b7defda70158392f6ee90e6a6e47ff15a4
gerball <gerball2193@gmail.com>
2024-09-02 18:32:39 +0100

    Application as done as I can do, Readme Finalising last up

--- fishDAO.py
+++ fishDAO.py
@@ -4,53 +4,76 @@ class FishDAO:
     def __init__(self):
         self.connection = sqlite3.connect('fishkeeping.db', check_same_thread=False)
         self.cursor = self.connection.cursor()
-        self.create_table()
+        self.create_fish_table()
+        self.create_tank_table()
 
-    def create_table(self):
-        self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS fish (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                species_name TEXT NOT NULL,
-                tank_name TEXT NOT NULL,
-                date_added DATE NOT NULL,
-                notes TEXT
-            )
-        ''')
+    def create_fish_table(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS fish (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                species_name TEXT NOT NULL,
+                                tank_name TEXT NOT NULL,
+                                date_added DATE NOT NULL,
+                                notes TEXT)''')
         self.connection.commit()
 
-    def getAll(self):
-        sql = "SELECT * FROM fish"
-        self.cursor.execute(sql)
-        results = self.cursor.fetchall()
-        return [self.convertToDictionary(result) for result in results]
-
-    def findByID(self, id):
-        sql = "SELECT * FROM fish WHERE id = ?"
-        self.cursor.execute(sql, (id,))
-        result = self.cursor.fetchone()
-        return self.convertToDictionary(result) if result else None
-
-    def create(self, fish):
-        sql = "INSERT INTO fish (species_name, tank_name, date_added, notes) VALUES (?, ?, ?, ?)"
-        self.cursor.execute(sql, (fish['species_name'], fish['tank_name'], fish['date_added'], fish['notes']))
-        self.connection.commit()
-        fish['id'] = self.cursor.lastrowid
-        return fish
-
-    def update(self, id, fish):
-        sql = "UPDATE fish SET species_name = ?, tank_name = ?, date_added = ?, notes = ? WHERE id = ?"
-        self.cursor.execute(sql, (fish['species_name'], fish['tank_name'], fish['date_added'], fish['notes'], id))
+    def create_tank_table(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS tanks (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                name TEXT NOT NULL,
+                                volume INTEGER,
+                                description TEXT)''')
         self.connection.commit()
 
-    def delete(self, id):
-        sql = "DELETE FROM fish WHERE id = ?"
-        self.cursor.execute(sql, (id,))
-        self.connection.commit()
+    # Fish CRUD operations
+    def get_all_fish(self):
+        self.cursor.execute("SELECT * FROM fish")
+        return [self.convert_to_dictionary(row, ['id', 'species_name', 'tank_name', 'date_added', 'notes']) for row in self.cursor.fetchall()]
 
-    def convertToDictionary(self, result):
-        colnames = ['id', 'species_name', 'tank_name', 'date_added', 'notes']
-        fish = {colnames[i]: result[i] for i in range(len(colnames))}
-        return fish
+    def add_fish(self, data):
+        self.cursor.execute('INSERT INTO fish (species_name, tank_name, date_added, notes) VALUES (?, ?, ?, ?)', (data['species_name'], data['tank_name'], data['date_added'], data['notes']))
+        self.connection.commit()
+        return self.cursor.lastrowid
+
+    def find_fish_by_id(self, id):
+        self.cursor.execute("SELECT * FROM fish WHERE id = ?", (id,))
+        return self.convert_to_dictionary(self.cursor.fetchone(), ['id', 'species_name', 'tank_name', 'date_added', 'notes'])
+
+    def update_fish(self, id, data):
+        self.cursor.execute('UPDATE fish SET species_name = ?, tank_name = ?, date_added = ?, notes = ? WHERE id = ?', (data['species_name'], data['tank_name'], data['date_added'], data['notes'], id))
+        self.connection.commit()
+        return True
+
+    def delete_fish(self, id):
+        self.cursor.execute('DELETE FROM fish WHERE id = ?', (id,))
+        self.connection.commit()
+        return True
+
+    # Tank CRUD operations
+    def add_tank(self, name, volume, description):
+        self.cursor.execute('INSERT INTO tanks (name, volume, description) VALUES (?, ?, ?)', (name, volume, description))
+        self.connection.commit()
+        return self.cursor.lastrowid
+
+    def get_all_tanks(self):
+        self.cursor.execute("SELECT * FROM tanks")
+        return [self.convert_to_dictionary(row, ['id', 'name', 'volume', 'description']) for row in self.cursor.fetchall()]
+
+    def find_tank_by_id(self, id):
+        self.cursor.execute("SELECT * FROM tanks WHERE id = ?", (id,))
+        return self.convert_to_dictionary(self.cursor.fetchone(), ['id', 'name', 'volume', 'description'])
+
+    def update_tank(self, id, data):
+        self.cursor.execute('UPDATE tanks SET name = ?, volume = ?, description = ? WHERE id = ?', (data['name'], data['volume'], data['description'], id))
+        self.connection.commit()
+        return True
+
+    def delete_tank(self, id):
+        self.cursor.execute('DELETE FROM tanks WHERE id = ?', (id,))
+        self.connection.commit()
+        return True
+
+    def convert_to_dictionary(self, row, keys):
+        return {keys[i]: row[i] for i in range(len(keys))} if row else None
 
     def closeAll(self):
         self.cursor.close()
--- server.py
+++ server.py
@@ -2,70 +2,70 @@ from flask import Flask, jsonify, request, render_template, abort
 from fishDAO import FishDAO
 
 app = Flask(__name__)
-
-# Initialize the DAO
-fishDAO = FishDAO()
+fishDAO = FishDAO()  # Initialize the DAO
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# API to get all fish
+# Fish Endpoints
 @app.route('/api/fish', methods=['GET'])
-def getAll():
-    results = fishDAO.getAll()
+def get_all_fish():
+    results = fishDAO.get_all_fish()
     return jsonify(results)
 
-# API to get a specific fish by ID
-@app.route('/api/fish/<int:id>', methods=['GET'])
-def findById(id):
-    foundFish = fishDAO.findByID(id)
-    if foundFish:
-        return jsonify(foundFish)
-    else:
-        abort(404)
-
-# API to create a new fish entry
 @app.route('/api/fish', methods=['POST'])
-def create():
-    if not request.json or not 'species_name' in request.json:
-        abort(400)
-    fish = {
-        "species_name": request.json['species_name'],
-        "tank_name": request.json['tank_name'],
-        "date_added": request.json['date_added'],
-        "notes": request.json.get('notes', '')
-    }
-    addedFish = fishDAO.create(fish)
-    return jsonify(addedFish), 201
+def add_fish():
+    data = request.json
+    fish_id = fishDAO.add_fish(data)
+    return jsonify({'id': fish_id}), 201
 
-# API to update an existing fish entry
+@app.route('/api/fish/<int:id>', methods=['GET'])
+def get_fish(id):
+    fish = fishDAO.find_fish_by_id(id)
+    return jsonify(fish) if fish else ('', 404)
+
 @app.route('/api/fish/<int:id>', methods=['PUT'])
-def update(id):
-    foundFish = fishDAO.findByID(id)
-    if not foundFish:
-        abort(404)
-    
-    if not request.json:
-        abort(400)
-    reqJson = request.json
-    if 'species_name' in reqJson:
-        foundFish['species_name'] = reqJson['species_name']
-    if 'tank_name' in reqJson:
-        foundFish['tank_name'] = reqJson['tank_name']
-    if 'date_added' in reqJson:
-        foundFish['date_added'] = reqJson['date_added']
-    if 'notes' in reqJson:
-        foundFish['notes'] = reqJson['notes']
-    
-    fishDAO.update(id, foundFish)
-    return jsonify(foundFish)
+def update_fish(id):
+    data = request.json
+    if fishDAO.update_fish(id, data):
+        return jsonify({"success": True})
+    return ('', 404)
 
-# API to delete a fish entry
 @app.route('/api/fish/<int:id>', methods=['DELETE'])
-def delete(id):
-    fishDAO.delete(id)
-    return jsonify({"done": True})
+def delete_fish(id):
+    if fishDAO.delete_fish(id):
+        return jsonify({"success": True})
+    return ('', 404)
+
+# Tank Endpoints
+@app.route('/api/tanks', methods=['GET'])
+def get_all_tanks():
+    return jsonify(fishDAO.get_all_tanks())
+
+@app.route('/api/tanks', methods=['POST'])
+def add_tank():
+    data = request.json
+    tank_id = fishDAO.add_tank(data['name'], data['volume'], data['description'])
+    return jsonify({'id': tank_id}), 201
+
+@app.route('/api/tanks/<int:id>', methods=['GET'])
+def get_tank(id):
+    tank = fishDAO.find_tank_by_id(id)
+    return jsonify(tank) if tank else ('', 404)
+
+@app.route('/api/tanks/<int:id>', methods=['PUT'])
+def update_tank(id):
+    data = request.json
+    if fishDAO.update_tank(id, data):
+        return jsonify({"success": True})
+    return ('', 404)
+
+@app.route('/api/tanks/<int:id>', methods=['DELETE'])
+def delete_tank(id):
+    if fishDAO.delete_tank(id):
+        return jsonify({"success": True})
+    return ('', 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
